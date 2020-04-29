c9f8faa3966186f9e0296cf94fc8660c5533ef1c
azvnit1207200 <62656728+azvnit1207200@users.noreply.github.com>
2020-04-29 00:15:35 +0000

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -45,11 +45,11 @@ function Facebook(accessToken, groupId)
         let formPublish = new FormData();
         let date = moment().format('dddd').toUpperCase() + ' NGÀY '+ moment().format('LL').toUpperCase();
         formPublish.append('message',
-            `# TIN TỨC BUỔI TRƯA ${date}\n` +
+            `# TIN TỨC BUỔI SÁNG ${date}\n` +
             '\n' +
             '## thông tin\n' +
-            '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS các bạn có thể ib cho admin [tại đây](https://facebook.com/ANHLTV2K3)');
+            '* Bài viết được tự động đăng lúc 7 giờ sáng hàng ngày\n' +
+            '* Đây là post tự động được tạo bởi GST COMMUNITY.');
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
